ac3dfd699107ac35bb44dd5ceeef8b3763106ebd
Moisés Souto <moises.souto@gmail.com>
2015-10-29 17:06:24 -0300

    Diminuição de um mês na contagem de tempo utilizando a classe time

--- aulas/aula05.rb
+++ aulas/aula05.rb
@@ -1,5 +1,7 @@
-t = Time.new(2012, 8, 22)
-puts t           #2012-08-22 00:00:00
-puts t + 60      #2012-08-22 00:01:00
+t = Time.new(2012, 8, 22)   #Criando instância da classe time atribuindo data
+puts t                      #2012-08-22 00:00:00
+puts t + 60                 #2012-08-22 00:01:00
 umdia = 60*60*24
-puts t - umdia   #2012-08-21 00:00:00
+ummes = 30 * umdia
+puts t - umdia              #2012-08-21 00:00:00
+puts t - ummes              #2012-07
