49f31831b32237b15dd3831abe7421bf61930e41
Moisés Souto <moises.souto@gmail.com>
2015-10-29 17:16:06 -0300

    Exibindo data, ano, mês

---
+++ aulas/aula05c.rb
@@ -0,0 +1,10 @@
+#Métodos para obtenção de informações da data:
+
+t = Time.new(2015, 10, 29)
+puts t.year   #ano -> 2015
+puts t.mon    #mes -> 10
+puts t.day    #dia do mes -> 29
+puts t.hour   #hora -> 0
+puts t.min    #minutos -> 0
+puts t.wday   #dia da semana (0 para domingo) -> 
+puts t.yday   #dia do ano -> 
