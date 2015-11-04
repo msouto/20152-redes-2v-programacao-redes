401b1c80cfabb114ac36b5b478012bdb75bac014
Moisés Souto <moises.souto@gmail.com>
2015-11-04 15:16:47 -0300

    Primeiro exemplo da aula, adicionando o inicio do segundo exemplo usando a classe time

---
+++ aulas/aula06.rb
@@ -0,0 +1,4 @@
+t = Time.new(2012, 10, 20, 13, 45, 0)
+puts t.strftime('%d/%m/%Y')       #20/10/2012
+puts t.strftime('%d-%m-%Y %H:%M') #20-10-2012 13:45
+puts t.strftime('%d de %B de %Y') #20 de October
