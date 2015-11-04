2f3b96b2f3d4021b8e3ccbf11d26ece740df4f1e
Moisés Souto <moises.souto@gmail.com>
2015-11-04 15:55:26 -0300

    Adicionando tratamento de exceção com exibição da exceção antigida

--- aulas/aula07.rb
+++ aulas/aula07.rb
@@ -1,7 +1,8 @@
 begin
   x=0
   y=5/x #codigo que pode conter condição de erro
-rescue Exception
+rescue Exception => ex #Atribuição de objeto de tratamento de erro
   y=5 #codigo de tratamento caso ocorra exceção
+  puts ex #impressao da mensagem de exceção
 end  
 puts y
