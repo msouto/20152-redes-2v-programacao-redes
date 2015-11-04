d02969a9b48203f8f5079d62ebdc2e9e0119b6e8
Moisés Souto <moises.souto@gmail.com>
2015-11-04 15:49:50 -0300

    Adicionando tratamente de exceção a aula 07

--- aulas/aula07.rb
+++ aulas/aula07.rb
@@ -1,4 +1,7 @@
-x=0
-y=5/x
-puts y #Teremos erro de divisão por zero
-       # Como tratar esse erro ?
+begin
+  x=0
+  y=5/x #codigo que pode conter condição de erro
+rescue Exception
+  y=5 #codigo de tratamento caso ocorra exceção
+end  
+puts y
