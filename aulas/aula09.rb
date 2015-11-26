3ec194f4340bdbe9e5f2450bdb8116f67ad7b62c
Moisés Souto <moises.souto@gmail.com>
2015-11-26 17:57:42 -0300

    Adicionando código de manipulação de linhas e contagem

--- aulas/aula09.rb
+++ aulas/aula09.rb
@@ -1,10 +1,14 @@
 #Arquivo de aula 09 - Programação para redes - 2015.2
 #Usando streams
 io = IO.new(STDOUT.fileno, "w")
-io.print "Bom dia"
+io.puts "Bom dia"
 
 
 #foreach(nome_arquivo){}: permite a definição de um bloco de código a ser executado a cada linha do arquivo. O exemplo abaixo imprime todas as linhas dos arquivo soneto.txt.
 IO.foreach("aula09_soneto.txt"){ |linha| 
 	puts linha
 }
+
+#retorna um array contendo as linhas de um arquivo. Cada elemento do array corresponde a uma linha do arquivo. readlines(nome_arquivo)
+a = IO.readlines("aula09_soneto.txt")
+puts a.size
