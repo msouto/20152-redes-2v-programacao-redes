d04d5e5e340e407454bd20c0491f8f5bd4ef2433
Moisés Souto <moises.souto@gmail.com>
2015-11-26 17:54:44 -0300

    Adicionando código de manipulação de arquivo de texto

--- aulas/aula09.rb
+++ aulas/aula09.rb
@@ -2,3 +2,9 @@
 #Usando streams
 io = IO.new(STDOUT.fileno, "w")
 io.print "Bom dia"
+
+
+#foreach(nome_arquivo){}: permite a definição de um bloco de código a ser executado a cada linha do arquivo. O exemplo abaixo imprime todas as linhas dos arquivo soneto.txt.
+IO.foreach("aula09_soneto.txt"){ |linha| 
+	puts linha
+}
