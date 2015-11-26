bf5a5f4aa77d0a745acffa4ae01ea517eba6ba84
Moisés Souto <moises.souto@gmail.com>
2015-11-26 18:01:44 -0300

    Adicionando exemplo de escrito em arquivo com a classe FIle

---
+++ aulas/aula10.rb
@@ -0,0 +1,7 @@
+#utilizando a classe File do Ruby
+f = File.new("aula10_trecho.txt", "w")
+f.puts "Mais um dia se levanta"
+f.puts "Na Jamaica brasileira"
+f.puts "Mais uma batalha que desperta"
+f.puts "A Nacao Regueira"
+f.close
