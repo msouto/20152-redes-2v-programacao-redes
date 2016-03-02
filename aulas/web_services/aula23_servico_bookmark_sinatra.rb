c3a8eb94b40445c1537c441a22d89188c8850e0f
Moisés Souto <moises.souto@gmail.com>
2016-03-02 16:08:19 -0300

    Criação da classe do usuário e manipulação dos links; Criação do tratamento as chamadas GET; Protótipo de chamada do metodo PUT

--- aulas/web_services/aula23_servico_bookmark_sinatra.rb
+++ aulas/web_services/aula23_servico_bookmark_sinatra.rb
@@ -6,6 +6,29 @@ include REXML
 #Para instalar o sinatra
 #gem install sinatra
 
+#variveis globais
+$contador = 1
+$usuarios = Hash.new
+
+class Usuario
+	attr_accessor :nome, :links
+
+	def initialize(nome)
+		@nome= nome
+		@links = Hash.new
+		@contador = 1
+	end
+
+	def adicionar_link(url)
+		@links[@contador] = url
+		@contador += 1
+	end
+
+	def remover_link(id_link)
+		@links.delete(id_link)
+	end
+
+end
 
 #porta padrão do sinatra 4567
 get '/' do
@@ -22,4 +45,8 @@ get '/usuario/:id' do
 		content_type 'text/xml'
 		"<?xml version='1.0'?><usuario nome='#{usuario.nome}'/>"
 	end
+end
+
+put '/usuario' do
+	#Codigo de tratamento para criação dos usuários
 end
