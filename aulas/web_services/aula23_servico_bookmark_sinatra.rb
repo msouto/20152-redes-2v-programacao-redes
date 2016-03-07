82413e9a1dba4ae84577666d570bac162a2d4f6a
Moisés Souto <moises.souto@gmail.com>
2016-03-07 14:21:04 -0300

    Aula com criacao de usuario via metodo PUT na API do sinatra

--- aulas/web_services/aula23_servico_bookmark_sinatra.rb
+++ aulas/web_services/aula23_servico_bookmark_sinatra.rb
@@ -1,52 +1,61 @@
 require "sinatra"
 require "rexml/document"
 include REXML
-#Implementação de serviço REST
-#Utiliz a biblioteca Sinatra, que é uma dependênica
-#Para instalar o sinatra
-#gem install sinatra
 
-#variveis globais
-$contador = 1
-$usuarios = Hash.new
+#Implementação de serviço REST.
+#Utiliza a biblioteca biblioteca Sinatra, a qual deve ser instalada previamente: 
+# gem install sinatra
 
-class Usuario
-	attr_accessor :nome, :links
-
-	def initialize(nome)
-		@nome= nome
-		@links = Hash.new
-		@contador = 1
-	end
-
-	def adicionar_link(url)
-		@links[@contador] = url
-		@contador += 1
-	end
-
-	def remover_link(id_link)
-		@links.delete(id_link)
-	end
-
-end
-
-#porta padrão do sinatra 4567
+#Utiliza 4567 como porta padrão
 get '/' do
-	'Olá Mundo!'
+  'Hello world'
 end
 
 get '/usuario/:id' do
-	id_usuario = params[:id]
-	usuario = $usuarios[id_usuario.to_i]
-	if usuario.nil?
-		status 404
-	else
-		status 200
-		content_type 'text/xml'
-		"<?xml version='1.0'?><usuario nome='#{usuario.nome}'/>"
-	end
+  id_usuario = params[:id]
+  usuario = $usuarios[id_usuario.to_i]
+  if usuario.nil?
+    status 404
+  else
+    status 200
+    content_type 'text/xml'
+    "<?xml version='1.0' ?><usuario nome='#{usuario.nome}'/>"
+  end
 end
 
 put '/usuario' do
-	#Codigo de tratamento para criação dos usuários
+  doc = Document.new(request.body.string)
+  nome = doc.elements[1].attributes["nome"]
+  if nome.nil?
+    status 404
+  else
+    usuario = Usuario.new(nome)
+    $usuarios[$contador] = usuario
+    puts ">>> Usuario criado com ID #{$contador}"
+    $contador += 1
+    status 200
+  end
 end
+
+class Usuario
+  attr_accessor :nome, :links
+  
+  def initialize(nome)
+    @nome = nome
+    @links = Hash.new
+    @contador = 1
+  end
+  
+  def adicionar_link(url)
+    @links[@contador] = url
+    @contador += 1
+  end
+  
+  def remover_link(id_link)
+    @links.delete(id_link)
+  end
+end
+
+#inicializa variáveis globais
+$contador = 1
+$usuarios = Hash.new
